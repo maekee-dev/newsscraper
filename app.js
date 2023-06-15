e256440854bcb961a8cedbdcbc5cfd9f1cbc801f
Diego <mantee99@outlook.com>
2023-06-15 11:29:03 +0200

    major update
    Removed axios and cheerio, now the scraping is made with puppeteer. The app fetch data from MongoDB, the scrap is done in async every hour and updated to the database

--- app.js
+++ app.js
@@ -1,79 +1,108 @@
-
-const express = require('express')
+const puppeteer = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 const path = require('path')
 const serveStatic = require('serve-static')
-const cheerio = require("cheerio")
-const axios = require("axios")
+const cron = require('node-cron')
+const { MongoClient } = require('mongodb');
+const express = require('express')
+const dotenv = require('dotenv');
 
-const getDate = () => {
-    const now = new Date();
-    const day = now.getDate().toString().padStart(2, '0');
-    const month = (now.getMonth() + 1).toString().padStart(2, '0');
-    const year = now.getFullYear();
-    const formattedDate = `${day}/${month}/${year}`;
-    return formattedDate
-}
+dotenv.config()
 
-const performScraping = async () => {    
-    const axiosResponse = await axios.request({
-        method: "GET",
-        url: "https://www.ilpost.it",
-        headers: {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+puppeteer.use(StealthPlugin())
+const url = 'https://www.ilpost.it'
+
+const scrapData = async() => {
+    const browser = await puppeteer.launch({ headless: "new" })
+    const page = await browser.newPage()
+    await page.goto(url)
+    const todayDate = getDate()
+    const postData = await page.evaluate((url, todayDate) => {
+        const data = {
+            title: document.querySelector('#content article .entry-content .entry-title a').innerHTML.trim(),
+            paragraph: document.querySelector('#content article .entry-content p a').innerHTML.trim(),
+            image: document.querySelector('#content article .entry-header img').getAttribute('src'),
+            link: document.querySelector('#content article .entry-header a').getAttribute('href'),
+            date: todayDate,
         }
-    })
-    const $ = cheerio.load(axiosResponse.data)
-    const article = $("#content").find("article").first()
-    const title = article.find(".entry-content").find(".entry-title").text()
-    const paragraph = article.find(".entry-content").find("p").text().trim()
-    const link = article.find("header").find("figure").find("a").attr("href")
-    const image = article.find("header").find("figure").find("a").find("img").attr("src")
-    const date = getDate()
-    const scrapedData = {
-        title: title,
-        paragraph: paragraph,
-        image: image,
-        link: link,
-        date: date,
-    }
-    return scrapedData
+        return data
+    }, url, todayDate)
+    await browser.close()
+    return postData
 }
 
+const dbConnect = async() => {
+    try{
+        const client = new MongoClient(
+            'mongodb+srv://' + process.env.DBUSR + ':' + process.env.DBPSW + '@cluster.qdcxe1q.mongodb.net/?retryWrites=true&w=majority',
+            { useNewUrlParser: true, useUnifiedTopology: true }
+        )
+        await client.connect()
+        return client
+    } catch(error){
+        console.error('Cannot connect to db:', error);
+        throw error
+    }
+}
 
-const app = express() 
-app.use(serveStatic(path.join(__dirname, 'public'))) 
+const getData = async() => {
+    let client
+    try{
+        client = await dbConnect()
+        const db = client.db('ScrapedArticles')
+        const collection = db.collection('Articles')
+        const data = await collection.findOne({}, { sort: { _id: -1 } })
+        delete data._id
+        return data
+    } 
+    catch(error){ console.error('Cannot read data:', error) } 
+    finally{ if(client) client.close() }
+}
+
+const writeData = async() => {
+    const data = await scrapData()
+    let client
+    try{
+        client = await dbConnect()
+        const db = client.db('ScrapedArticles')
+        const collection = db.collection('Articles')
+        const isOld = await collection.findOne({ title: data.title });
+        if (!isOld) await collection.insertOne(data)
+    } 
+    catch(error){ console.error('Cannot write data:', error) }
+    finally{ if(client) client.close() }
+}
+
+const app = express()
+const port = process.env.PORT || 8080
+app.use(serveStatic(path.join(__dirname, '/public'))) 
 app.set('views', __dirname + '/public/view/');
 app.set('view engine', 'ejs');
 
-app.get('', (req, res) => {
-    performScraping()
-    .then((resultData) => {
-        res.render('index', { data: resultData })
-    })
-    .catch((error) => {
-        resultData = {
-            title: 'error',
-            paragraph: 'error',
-            image: 'error',
-            link: 'error',
-            date: 'error'
-        }
-        res.render('index', { data: resultData })
-    })
+const deployData = async(_req, res, next) => {
+    res.locals.data = await getData()
+    next()
+}
+
+app.get('/', deployData, (_req, res) => {
+    res.render('index', { data: res.locals.data })
 })
 
-app.get('/json', (req, res) => {
-    performScraping()
-    .then((resultData) => {
-        res.json(resultData)
-    })
-    .catch((error) => {
-        console.error(error)
-        res.json( { error: 'Si è verificato un errore durante lo scraping dei dati.' })
-    })
+app.get('/json', deployData, (_req, res) => {
+    res.json(res.locals.data)
 })
 
-const PORT = process.env.PORT || 4000
-app.listen(PORT, () => console.info(`App listening on port ${PORT}`))
+app.listen(port, () => {
+    return console.log(`Server is listening on ${port}`)
+})
 
-module.exports = app
+const getDate = () => {
+    const now = new Date()
+    const day = now.getDate().toString().padStart(2, '0')
+    const month = (now.getMonth() + 1).toString().padStart(2, '0')
+    const year = now.getFullYear()
+    const formattedDate = day + '/' + month + '/' + year
+    return formattedDate
+}
+
+cron.schedule('0 * * * *', () => writeData())
